e9dfa68b166aa8ea89679faea01fd8232aeab135
Eva-97 <evaristus.eke97@gmail.com>
2023-06-18 10:57:11 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *
+ * main - Able to print
+ * Return: (0) has to be successful
+ *
+ */
+
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
