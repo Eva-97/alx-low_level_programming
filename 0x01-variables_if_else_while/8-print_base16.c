6165b915b47fa2e3aa1a632698483b04e962109e
Eva-97 <evaristus.eke97@gmail.com>
2023-06-20 13:29:44 -0700

    git commit -m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Programme that prints all the numbers in base sixteen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	unsigned int hexadecimal_number = 48;
+	char hexadecimal_alphabet = 'a';
+
+	while (hexadecimal_number <= 57)
+	{
+		putchar(hexadecimal_number);
+		hexadecimal_number++;
+	}
+	while (hexadecimal_alphabet <= 'f')
+	{
+		putchar(hexadecimal_alphabet);
+		hexadecimal_alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
