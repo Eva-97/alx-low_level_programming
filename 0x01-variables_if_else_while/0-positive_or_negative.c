ade1d83882f25fb80e96660e7478fb95aef4b898
Eva-97 <evaristus.eke97@gmail.com>
2023-06-19 20:30:25 -0700

    Updated positive and negative source code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,30 @@
-#include <unistd.h>
-#include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Print if source code is postive or negative
+ * main - Print positive or negative for stored number
  *
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
-	else
+	}
+	else if (n == 0)
+	{
 		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
 	return (0);
 }
