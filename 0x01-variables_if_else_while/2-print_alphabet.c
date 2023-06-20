4092392a638a22160adc9d83f65d506de057b2fd
Eva-97 <evaristus.eke97@gmail.com>
2023-06-20 12:23:02 -0700

    Update print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet game
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int char = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
