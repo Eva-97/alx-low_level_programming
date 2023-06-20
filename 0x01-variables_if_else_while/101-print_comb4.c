960c889d6fe03fff310cdabba46406f1bddcc976
Eva-97 <evaristus.eke97@gmail.com>
2023-06-20 13:54:01 -0700

    program that prints all different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int firstnum, secondnum, thirdnum;
+
+	firstnum = 48;
+	while (firstnum < 58)
+	{
+		secondnum = 49;
+		while (secondnum < 58)
+		{
+			thirdnum = 50;
+			while (thirdnum < 58)
+			{
+				if (thirdnum > secondnum && secondnum > firstnum)
+				{
+					putchar(firstnum);
+					putchar(secondnum);
+					putchar(thirdnum);
+					if (firstnum != 55 || secondnum != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				thirdnum++;
+			}
+			secondnum++;
+		}
+		firstnum++;
+	}
+	putchar('\n');
+	return (0);
+}
