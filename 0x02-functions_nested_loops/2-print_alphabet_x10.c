5cbea8b3584830ebf079e904db51874d63b2b09f
Eva-97 <evaristus.eke97@gmail.com>
2023-06-20 21:55:33 -0700

    Print alphabet updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 /**
 *main - Function that prints ten times the alphabet in lowercase
 *Return: Always 0
-*
 */
-void print_alphabet_x10(void)
 
+void print_alphabet_x10(void)
 {
 int counter = 0;
 int letter;
 while (counter++ <= 9)
+
 {
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
