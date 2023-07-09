1e6c17438171400ce46b81647b16847d48026689
Eva-97 <evaristus.eke97@gmail.com>
2023-07-09 16:20:54 -0700

    A Library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @s: the character to be checked
+ * Return: 1 if s is uppercase, 0 otherwise
+ */
+int _isupper(int s)
+{
+	return (s >= 'A' && s <= 'Z');
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @s: the character to check
+ * Return: 1 if s is lowercase, 0 otherwise
+ */
+int _islower(int s)
+{
+	return (s >= 'a' && s <= 'z');
+}
