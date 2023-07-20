729adc5b94189fa7d4d13e502195a3500ab5f321
Eva-97 <evaristus.eke97@gmail.com>
2023-07-20 15:53:19 -0700

    Print dog updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,18 +1,26 @@
-#include <stdio.h>
 #include "dog.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+ * print_dog - this function print a dog.
+ * @d: the struct of the dog
+ (* a blank line
+* Description: this function print a dog)?
+(* section header: the header of this function is dog.h)*
+* Return: a void function no return.
+*/
+void print_dog(struct dog *d)
 {
-	struct dog my_dog;
-
-	my_dog.name = "Bingo";
-	my_dog.age = 2.5;
-	my_dog.owner = "Eva";
-	print_dog(&my_dog);
-	return (0);
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+	else
+		return;
 }
